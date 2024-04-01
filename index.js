5084b7bb54a82d671c1a0304fdcf6fde74e0c026
Brigid Wanza Wambua <wanzanashami@gmail.com>
2024-04-01 10:53:45 +0300

    feat: very interesting

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000, 100); // =>
+
+const square = x => x ** 2;
+const nums = [2000,100];
+nums;//=>[2000, 100]
+
+const add = (a, b) => a + b;
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
+sum(2000, 100);//=>
+
+
